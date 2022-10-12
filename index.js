cf5965c964fa6030c411d9eeb9b87a502f84a15a
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-12 08:12:14 -0700

    commit tree 31518e5f20db89bdd1d36df784ef685ce51bbbe8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+  return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun());
+
+function mondayWork(activity = "go to the office"){
+  return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+
+function wrapAdjective(very = "special"){
+  return `You are ${very}!`;
+  
+}
+console.log(wrapAdjective);
+
